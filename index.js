aa442aeaae6c5cfff7d6be27539a09ea411a176d
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-18 21:28:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function depthFirstSearch(rootNode, vertices, edges) {
   let visitedVertices = []
   vsitedVertices.push(rootNode)
 }
+
+
+function findAdjacent(firstNode, vertices, edges) {
+  let adjEdge = edges.filter(x => x.includes(`${firstNode}`))
+  let adjArray = adjEdge.map(x => x.find(function(element) {return element != firstNode}))
+  return vertices.filter(x => adjArray.includes(x.name) && x.discovered === null)
+}
