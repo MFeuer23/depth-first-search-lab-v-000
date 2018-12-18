c6bc346487feb74eb6a08ee899a1ad0c820d9fa9
Mallory Feuer <mallory.feuer@gmail.com>
2018-12-18 21:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function depthFirstSearch(rootNode, vertices, edges) {
   let discovered = [rootNode]
   let discoverOrder = [rootNode]
   while(discovered.length != 0){
-    let currentNode = discovered.shift()
+    let currentNode = discovered.pop()
     let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
     discoverOrder = discoverOrder.concat(adjacentNodes);
     discovered = discovered.concat(adjacentNodes)
